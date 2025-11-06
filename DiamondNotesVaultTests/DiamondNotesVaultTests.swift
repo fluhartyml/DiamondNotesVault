1d44c6ca1dc61096481cae89c575ccb714bb3c01
Michael Fluharty <michael.fluharty@mac.com>
2025-11-06 09:25:43 -0600

    Initial Commit

---
+++ DiamondNotesVaultTests/DiamondNotesVaultTests.swift
@@ -0,0 +1,16 @@
+//
+//  DiamondNotesVaultTests.swift
+//  DiamondNotesVaultTests
+//
+//  Created by Michael Fluharty on 11/6/25.
+//
+
+import Testing
+
+struct DiamondNotesVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
